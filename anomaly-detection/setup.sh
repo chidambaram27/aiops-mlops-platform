4cae044fd4338a1abbd8a048641c171f319d1a4d
chidambaram27 <chidambaramk.27@gmail.com>
2025-12-28 17:12:41 +0530

    anomaly tool updated

--- anomaly-detection/setup.sh
+++ anomaly-detection/setup.sh
@@ -4,10 +4,16 @@
 kubectl apply dummy_pod.yaml
 
 # update the DEFAULT_INSTANCE_IP set to the t3.small instance
-docker buildx build --platform linux/amd64 -t chidambaram27/anomaly-detection-tool:v1 ./tool
+docker buildx build --platform linux/amd64 -t chidambaram27/anomaly-detection-tool:v2 ./tool
 
 docker push chidambaram27/anomaly-detection-tool:v1
 
 # deploy model_tool as fastmcp server for KAgent
 # update INFERENCE_SERVICE_URL, MODEL_NAME, DEFAULT_INSTANCE_IP
 kubectl apply -f model_tool.yaml
+
+# deploy KAgent RemoteMCPServer
+kubectl apply -f mcpserver.yaml
+
+# deploy Agent
+kubectl apply -f agent.yaml
